efd86c6bd8078a6f42e54df4b64c309beaa4da6e
EliteWebmaster <44509681+elitewebmaster@users.noreply.github.com>
2023-01-03 21:35:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { withCookies } from "react-cookie";
+import DatePicker from "react-datepicker";
+import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
 
-function App() {
+const defaultGoalDate = () => {
+  var CurrentDate = new Date();
+  CurrentDate.setMonth(CurrentDate.getMonth() + 1);
+  return CurrentDate;
+};
+
+const App = (props) => {
+  const { cookies } = props;
+  const [goalDate, setGoalDate] = useState(
+    new Date(cookies.get("goalDate") || defaultGoalDate())
+  );
+
+  let oneDay = 24 * 60 * 60 * 1000,
+    firstDate = new Date(),
+    diffDays = Math.round(
+      Math.abs((firstDate.getTime() - goalDate.getTime()) / oneDay)
+    );
+
+  while (firstDate <= goalDate) {
+    firstDate = new Date(+firstDate + oneDay);
+  }
+
+  const handleChange = (date) => {
+    if (date !== null) {
+      setGoalDate(date);
+      cookies.set("goalDate", date, {
+        path: "/",
+        expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+      });
+    } else {
+      setGoalDate(defaultGoalDate());
+
+      cookies.set("goalDate", this.defaultGoalDate(), {
+        path: "/",
+        expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+      });
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Countdown</h1>
+        <div className="row">
+          <label htmlFor="datepicker">Date:</label>
+          <DatePicker
+            id="datepicker"
+            selected={goalDate}
+            onChange={handleChange}
+            minDate={new Date()}
+            isClearable={false}
+            showDisabledMonthNavigation
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>{diffDays} Days left</strong>
+          <br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default withCookies(App);
